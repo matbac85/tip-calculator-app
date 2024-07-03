111d2593117fd05b3e35d0f2a32547716fa7f4a4
matbac85 <mathildebaclin@gmail.com>
2024-07-03 12:05:24 +0200

    added an H1 with the .visually-hidden class designed by Orange

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <h1 class="visually-hidden">Tip Calculator App</h1>
     <Logo />
     <Form />
   </main>
@@ -20,6 +21,18 @@ main {
   align-items: center;
   gap: 2.5rem;
   box-sizing: border-box;
+}
+
+.visually-hidden {
+  position: absolute !important;
+  width: 1px !important;
+  height: 1px !important;
+  padding: 0 !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  clip: rect(0, 0, 0, 0) !important;
+  white-space: nowrap !important;
+  border: 0 !important;
 }
 
 @media (min-width: 37.5rem) {
